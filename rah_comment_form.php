9a65d62b1021c681a451bd22365d77b1e1446d2c
Jukka Svahn <web@rahinaa.biz>
2012-08-14 12:11:21 +0300

    Simulate zero fill.
    Fixed redirect location.

--- rah_comment_form.php
+++ rah_comment_form.php
@@ -303,7 +303,7 @@ class rah_comment_form {
 		callback_event(__CLASS__.'.saved', '', false, $this->form);
 		update_comments_count($this->form->parent);
 		
-		header('Location: '.permlink(array()).'#c'.$comment);
+		header('Location: '.permlink(array()).'#c'.str_pad($comment, 6, '0', STR_PAD_LEFT));
 	}
 }
 	
